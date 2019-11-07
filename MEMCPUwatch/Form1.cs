86c4d7d86950338cfbeed2395f0a0a843733e013
Jason Jointer <jason.jointer@nationsnetwork.com>
2019-11-06 17:38:30 -0800

    Add project files.

---
+++ MEMCPUwatch/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MEMCPUwatch
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer_tick(object sender, EventArgs e)
+        {
+            float fcpu = pCPU.NextValue();
+            float fram = pRAM.NextValue();
+            //Set value to cpu and ram
+            metroProgressBarCPU.Value = (int)fcpu;
+            metroProgressBarRAM.Value = (int)fram;
+            //Update value to cpu and ram label
+            lblCPU.Text = string.Format("{0:0.00}%", fcpu);
+            lblRAM.Text = string.Format("{0:0.00}%", fram);
+            //Draw cpu and ram chart
+            chart1.Series["CPU"].Points.AddY(fcpu);
+            chart1.Series["RAM"].Points.AddY(fram);
+        }
+
+        private void MetroLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MetroProgressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MetroLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+    }
+}
